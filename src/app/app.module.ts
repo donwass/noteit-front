3d4f5380ea832ffe06ada9dec7aacb328b390d1c
zaiats <azdronik@gmail.com>
2019-12-21 23:32:38 +0200

    wrote front-end for feedback page. also wrote navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,53 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+
+const appRoutes: Routes = [
+  {
+    // localhost:4200/notes
+    path: "notes",
+    component: NotesComponent
+  },
+  {
+    // localhost:4200/feedback
+    path: "feedback",
+    component: FeedbackComponent
+  },
+  {
+    // localhost:4200/
+    path: "",
+    component: NotesComponent,
+    pathMatch: "full"
+  },
+  {
+    // localhost:4200/asdf
+    path: "**",
+    component: NotFoundComponent
+  }
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FeedbackComponent,
+    NotesComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
