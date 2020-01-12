ae4b8cdcfe0fab8330488cb3d4d71adec627eabd
Max <azdronik@gmail.com>
2020-01-12 13:15:42 +0200

    updated

--- src/app/notes/note/note.component.ts
+++ src/app/notes/note/note.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-note',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteComponent implements OnInit {
 
-  note: Note;
+  @Input() note: Note;
 
   constructor() { }
 
