a57c9a7777e4c0c97dbb9045817fd2ad06e5fc10
Max <azdronik@gmail.com>
2020-01-26 11:33:32 +0200

    created pipe for filter

---
+++ src/app/shared/note-text-filter.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'noteTextFilter'
+})
+export class NoteTextFilterPipe implements PipeTransform {
+
+  transform(notes:Note[], text: string): Note[] {
+    if(text == null || text === ""){
+      return notes;
+    }
+    return notes.filter(n => n.title.includes(text) || n.text.includes(text));
+  }
+
+}
